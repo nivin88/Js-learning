c03245ba952ef017f12bb5f1dd279ae46d20628c
nivin88 <nivinkamaraj@gmail.com>
2024-10-17 17:23:04 +0530

    first commit

--- intex.js
+++ intex.js
@@ -1,3 +1,22 @@
+function mySubmit(event) {
+    
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    console.log('Name:', username);
+    console.log('Password:', password);
+
+
+   
+    let dataText = `<h2>Data</h2>
+        <p><b>Username</b>:${username}</p>
+        <p><b>Password</b>:${password}</p>`;
+        let result = document.getElementById('result');
+        result.innerHTML = dataText; 
+        result.style.paddingTop = "2rem";
+}
+// Increment
 var a=45;
 ++a;
 console.log(a);
@@ -6,11 +25,12 @@ var a=30;
 b=a++;
 console.log(b);
 
+// Decrement
 var a=70;
 b=--a;
 console.log(b);
 
-
+// The else Statement
 var car=false
 if (car)
 {
@@ -20,19 +40,22 @@ else{
     console.log("go an take the key")
     }
 
-
+// The else if Statement
     var score = 100;
 
     if (score < 70) {
         console.log("You need to improve");
     } else if (score <= 80) {
-        console.log("Good job");
+        console.log("Good ");
     } else if (score > 80) {
         console.log("Excellent performance");
     }
     else{
       console.log("fail");
     }
+
+
+    
     
 
     
