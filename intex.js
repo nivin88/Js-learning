bbd8101b1e2ceb7b72a43f537856692ee2626fa2
nivin88 <nivinkamaraj@gmail.com>
2024-10-18 17:13:35 +0530

    first commit

--- intex.js
+++ intex.js
@@ -55,6 +55,68 @@ else{
     }
 
 
+    //while loop
+let i = 0;
+
+while (i < 15) {
+    console.log(i);
+    i++;
+}
+
+//do while
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i < 15);
+
+ // Destructuring Method:
+const fruits = ['Graps', 'apple', 'orange'];
+const [first, second, third] = fruits;
+console.log(first);
+
+// //Array Method:
+let color = ['blue','orange','Green'];
+// for(let key in color){
+//   console.log(color [key]);
+// }
+
+//for of method:
+for(let colors of color){
+  console.log("colors:"+ color);
+}
+
+
+ //for loop
+ for(let i=0;i<10;i++)
+  console.log(i);
+
+ //for in loop
+ //object method:
+//  const person = { name: 'Peavin', age: 25, place: 'Orathanadu' };
+// for (let key in person) {
+//   //concat method
+//     console.log(key + ': ' + person[key]);
+// }
+
+
+//continue
+for (let i = 0; i < 12; i++) {
+    if (i === 5) {
+        continue; 
+    }
+    console.log(i);
+  }
+
+//break
+for (let i = 0; i < 20; i++) {
+  if (i === 8) {
+      break; 
+  }
+  console.log(i);
+}
+
+
     
     
 
